376ffda0e4a300a9224e1f50f7d6ad7ba192f3d1
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-10-16 13:05:53 -0400

    Refactoring.

--- src/model/readout.rs
+++ src/model/readout.rs
@@ -6,54 +6,65 @@ use bio::stats::combinatorics::combinations;
 use bio::stats::logprobs::{Prob, LogProb};
 
 
-/// Probability to make exactly i 1->0 and j 0->1 errors.
-fn xi(i: u8, j: u8, N: u8, m: u8, p0: Prob, p1: Prob) -> Prob {
-    p1.powi(i as i32) * p0.powi(j as i32) * (1.0 - p1).powi((m - i) as i32) * (1.0 - p0).powi((N - m - j) as i32)
-}
-
-/// Number of possibilities for i 1->0 and j 0->1 errors.
-fn psi(i: u8, j: u8, N: u8, m: u8) -> f64 {
-    combinations(m as u64, i as u64) * combinations((N - m)  as u64, j as u64)
+struct Factory {
+    N: u8,
+    m: u8,
+    p0: Prob,
+    p1: Prob
 }
 
 
-/// Probability to see an exact readout given that we have a call.
-fn prob_call_exact(N: u8, m: u8, p0: Prob, p1: Prob) -> Prob {
-    xi(0, 0, N, m, p0, p1)
-}
+impl Factory {
 
-
-/// Probability to see a readout with one mismatch given that we have a call.
-fn prob_call_mismatch(N: u8, m: u8, p0: Prob, p1: Prob) -> Prob {
-    (m as f64 * xi(1, 0, N, m, p0, p1) + (N - m) as f64 * xi(0, 1, N, m, p0, p1)) / N as f64
-}
-
-
-/// Probability to see an exact readout given that we have a miscall.
-fn prob_miscall_exact(N: u8, m: u8, p0: Prob, p1: Prob) -> Prob {
-    xi(2, 2, N, m, p0, p1)
-}
-
-
-/// Probability to see a readout with one mismatch given that we have a miscall.
-fn prob_miscall_mismatch(N: u8, m: u8, p0: Prob, p1: Prob) -> Prob {
-    (psi(2, 1, N, m) * xi(2, 1, N, m, p0, p1) + psi(1, 2, N, m) * xi(1, 2, N, m, p0, p1) +
-    psi(2, 3, N, m) * xi(2, 3, N, m, p0, p1) + psi(3, 2, N, m) * xi(3, 2, N, m, p0, p1)) /
-    (psi(2, 1, N, m) + psi(1, 2, N, m) + psi(2, 3, N, m) + psi(3, 2, N, m))
-}
-
-
-/// Probability to completely miss a readout because of too many errors.
-fn prob_missed(N: u8, m: u8, p0: Prob, p1: Prob) -> Prob {
-    let mut num = 0.0;
-    let mut denom = 0.0;
-    for k in 3..6 {
-        for i in 0..cmp::min(m, k) + 1 {
-            num += psi(i, k - i, N, m) * xi(i, k - i, N, m, p0, p1);
-            denom += psi(i, k - i, N, m);
-        }
+    /// Probability to make exactly i 1->0 and j 0->1 errors.
+    fn xi(&self, i: u8, j: u8) -> Prob {
+        self.p1.powi(i as i32) * self.p0.powi(j as i32) * (1.0 - self.p1).powi((self.m - i) as i32) * (1.0 - self.p0).powi((self.N - self.m - j) as i32)
     }
-    num / denom
+
+    /// Number of possibilities for i 1->0 and j 0->1 errors.
+    fn psi(&self, i: u8, j: u8) -> f64 {
+        combinations(self.m as u64, i as u64) * combinations((self.N - self.m)  as u64, j as u64)
+    }
+
+
+    /// Probability to see an exact readout given that we have a call.
+    fn prob_call_exact(&self) -> Prob {
+        self.xi(0, 0)
+    }
+
+
+    /// Probability to see a readout with one mismatch given that we have a call.
+    fn prob_call_mismatch(&self) -> Prob {
+        (self.m as f64 * self.xi(1, 0) + (self.N - self.m) as f64 * self.xi(0, 1)) / self.N as f64
+    }
+
+
+    /// Probability to see an exact readout given that we have a miscall.
+    fn prob_miscall_exact(&self) -> Prob {
+        self.xi(2, 2)
+    }
+
+
+    /// Probability to see a readout with one mismatch given that we have a miscall.
+    fn prob_miscall_mismatch(&self) -> Prob {
+        (self.psi(2, 1) * self.xi(2, 1) + self.psi(1, 2) * self.xi(1, 2) +
+        self.psi(2, 3) * self.xi(2, 3) + self.psi(3, 2) * self.xi(3, 2)) /
+        (self.psi(2, 1) + self.psi(1, 2) + self.psi(2, 3) + self.psi(3, 2))
+    }
+
+
+    /// Probability to completely miss a readout because of too many errors.
+    fn prob_missed(&self) -> Prob {
+        let mut num = 0.0;
+        let mut denom = 0.0;
+        for k in 3..6 {
+            for i in 0..cmp::min(self.m, k) + 1 {
+                num += self.psi(i, k - i) * self.xi(i, k - i);
+                denom += self.psi(i, k - i);
+            }
+        }
+        num / denom
+    }
 }
 
 
@@ -69,12 +80,13 @@ pub struct Readout {
 
 impl Readout {
     pub fn new(N: u8, m: u8, p0: Prob, p1: Prob) -> Self {
+        let factory = Factory { N: N, m: m, p0: p0, p1: p1 };
         Readout {
-            prob_call_exact: prob_call_exact(N, m, p0, p1).ln(),
-            prob_call_mismatch: prob_call_mismatch(N, m, p0, p1).ln(),
-            prob_miscall_exact: prob_miscall_exact(N, m, p0, p1).ln(),
-            prob_miscall_mismatch: prob_miscall_mismatch(N, m, p0, p1).ln(),
-            prob_missed: prob_missed(N, m, p0, p1).ln()
+            prob_call_exact: factory.prob_call_exact().ln(),
+            prob_call_mismatch: factory.prob_call_mismatch().ln(),
+            prob_miscall_exact: factory.prob_miscall_exact().ln(),
+            prob_miscall_mismatch: factory.prob_miscall_mismatch().ln(),
+            prob_missed: factory.prob_missed().ln()
         }
     }
 }
@@ -84,7 +96,7 @@ impl Readout {
 mod tests {
     #![allow(non_upper_case_globals)]
 
-    use super::{prob_call_exact, prob_call_mismatch, prob_miscall_exact, prob_miscall_mismatch, prob_missed, xi};
+    use super::{Factory};
     use bio::stats::logprobs::Prob;
     use nalgebra::ApproxEq;
 
@@ -92,55 +104,57 @@ mod tests {
     const m: u8 = 4;
     const p0: Prob = 0.04;
     const p1: Prob = 0.1;
+    const factory: Factory = Factory { N: 16, m: 4, p0: 0.04, p1: 0.1 };
+
 
     #[test]
     fn test_prob_call_exact() {
-        let p = prob_call_exact(N, m, p0, p1);
+        let p = factory.prob_call_exact();
         println!("{}", p);
         assert!(p.approx_eq(&0.4019988717840602));
     }
 
     #[test]
     fn test_prob_call_mismatch() {
-        let p = prob_call_mismatch(N, m, p0, p1);
+        let p = factory.prob_call_mismatch();
         println!("{}", p);
         assert!(p.approx_eq(&0.0237291000705869));
     }
 
     #[test]
     fn test_prob_miscall_exact() {
-        let p = prob_miscall_exact(N, m, p0, p1);
+        let p = factory.prob_miscall_exact();
         println!("{}", p);
         assert!(p.approx_eq(&0.000008616230962449854));
     }
 
     #[test]
     fn test_prob_miscall_mismatch() {
-        let p = prob_miscall_mismatch(N, m, p0, p1);
+        let p = factory.prob_miscall_mismatch();
         println!("{}", p);
         assert!(p.approx_eq(&0.00001879564966027473));
     }
 
     #[test]
     fn test_prob_missed() {
-        let p = prob_missed(N, m, p0, p1);
+        let p = factory.prob_missed();
         println!("{}", p);
         assert!(p.approx_eq(&0.000007861209464082392));
     }
 
     #[test]
     fn test_xi() {
-        let p = xi(0, 0, N, m, p0, p1);
+        let p = factory.xi(0, 0);
         assert!(p.approx_eq(&0.4019988717840602));
-        let p = xi(1, 0, N, m, p0, p1);
+        let p = factory.xi(1, 0);
         assert!(p.approx_eq(&0.04466654130934002));
-        let p = xi(0, 1, N, m, p0, p1);
+        let p = factory.xi(0, 1);
         assert!(p.approx_eq(&0.01674995299100251));
-        let p = xi(2, 2, N, m, p0, p1);
+        let p = factory.xi(2, 2);
         assert!(p.approx_eq(&8.616230962449852e-06));
-        let p = xi(2, 1, N, m, p0, p1);
+        let p = factory.xi(2, 1);
         assert!(p.approx_eq(&0.00020678954309879646));
-        let p = xi(1, 2, N, m, p0, p1);
+        let p = factory.xi(1, 2);
         assert!(p.approx_eq(&7.754607866204867e-05));
     }
 }
