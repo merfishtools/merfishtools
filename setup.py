bfc819cc95884249d1ad043b9af8a39994c250a2
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-05-09 14:24:09 +0200

    Add Python setup script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Merfishtools',
+      version='1.0.0',
+      description='This library complements the MERFISHtools command line utility.',
+      author='Johannes Köster',
+      author_email='johannes.koester@tu-dortmund.de',
+      url='http://merfishtools.github.io',
+      packages=['merfishtools'])
