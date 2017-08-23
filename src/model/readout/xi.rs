c58b6124608983dea4b2a48ecab1f12f9db95481
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-08-23 15:03:27 +0200

    More unit tests.

--- src/model/readout/xi.rs
+++ src/model/readout/xi.rs
@@ -53,3 +53,58 @@ impl Xi {
         [prev[1], prev[2]]
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use bit_vec::BitVec;
+    use bio::stats::{Prob, LogProb};
+
+
+    #[test]
+    fn test_xi_exact_or_mismatch() {
+        let a = BitVec::from_bytes(&[0b10101101]);
+        let b = BitVec::from_bytes(&[0b10101011]);
+        let p0 = LogProb::from(Prob(0.005));
+        let p1 = LogProb::from(Prob(0.01));
+
+        let xi = Xi::new(&[Prob::from(p0); 8], &[Prob::from(p1); 8]);
+
+        let p = xi.prob(&a, &b);
+
+        let truth = LogProb(*p1.ln_one_minus_exp() * 4.0) +
+                    LogProb(*p0.ln_one_minus_exp() * 2.0) +
+                    p1 + p0;
+
+        assert_relative_eq!(*p[0], *truth);
+
+        let truth_mismatch = LogProb::ln_sum_exp(&[
+            truth - p1.ln_one_minus_exp() + p1, // mismatch in matching 1-bits
+            truth - p0.ln_one_minus_exp() + p0, // mismatch in matching 0-bits
+            truth - p1 + p1.ln_one_minus_exp(), // mismatch in mismatching 1-bits
+            truth - p0 + p0.ln_one_minus_exp()  // mismatch in mismatching 0-bits
+        ]);
+
+        assert_relative_eq!(*p[1], *truth_mismatch, epsilon=0.001);
+    }
+
+    #[test]
+    fn test_xi_exact_total() {
+        let p0 = LogProb::from(Prob(0.005));
+        let p1 = LogProb::from(Prob(0.01));
+        let xi = Xi::new(&[Prob::from(p0); 8], &[Prob::from(p1); 8]);
+
+        let a = BitVec::from_bytes(&[0b10101101]);
+
+        let mut probs = Vec::new();
+        for b in 0..256 {
+            let b = BitVec::from_bytes(&[b]);
+            probs.push(xi.prob(&a, &b)[0]);
+        }
+        let p = LogProb::ln_sum_exp(&probs);
+
+        assert_relative_eq!(*p, *LogProb::ln_one());
+    }
+}
