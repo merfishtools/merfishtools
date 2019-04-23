53b621ea99a848893d712f82c7bea3169e6cd947
Till Hartmann <till.hartmann@udo.edu>
2019-04-23 15:49:23 +0200

    do not write hamming.rs if it already exists; workaround

--- build.rs
+++ build.rs
@@ -13,8 +13,11 @@ fn bit_count(a: usize) -> usize {
 
 fn main() -> std::io::Result<()> {
     let nbits: Vec<usize> = (0..2 << (16 - 1)).map(|i| bit_count(i)).collect();
+    if fs::File::open("src/model/la/hamming.rs").is_ok() {
+        return Ok(());
+    }
     let mut f = fs::File::create("src/model/la/hamming.rs")?;
-    f.write_all(b"const _NBITS16: [usize; 65536] = [")?;
+    f.write_all(b"pub const _NBITS16: [usize; 65536] = [")?;
     f.write(b"    ")?;
     for (i, n) in nbits.iter().enumerate() {
         if i % 16 == 0 {
@@ -23,11 +26,13 @@ fn main() -> std::io::Result<()> {
         f.write_fmt(format_args!("{}, ", n))?;
     }
     f.write(b"];\n")?;
-    f.write(b"
+    f.write(
+        b"
 pub fn hamming_distance16(a: usize, b: usize) -> usize {
     let x = a ^ b;
     _NBITS16[x]
- }")?;
+ }",
+    )?;
     f.sync_all()?;
     Ok(())
 }
